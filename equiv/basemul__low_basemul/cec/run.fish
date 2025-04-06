9c07700b7d87632e5f02627276316a82550c7cff
jammychiou1 <jammy.chiou1@gmail.com>
2025-04-06 15:34:22 +0800

    add basemul_low cec time logs of 2 versions
    1.  O3 before O1, using default cec engine. See `run.fish` and logs in
        `all_mids_with_mm_with_arr__O3O1/`.
    2.  O3 before O1, using patched-in &cec-two engine option for the radix2
        cases with flags `-C 1000000`. (This engine breaks for Karatsuba,
        which might be caused by the "miter vs &miter"/"cec vs &cec" issue
        in abc [https://github.com/berkeley-abc/abc/issues/259].) See
        `cectwo_run.fish` and logs in `all_mids_with_mm_with_arr__O3O1/`.
    
    Both use all mids (0123456) and mm hint variables. For the worst testcases,
    in both versions, O3 before O1 is much faster in than the opposite order.

---
+++ equiv/basemul__low_basemul/cec/run.fish
@@ -0,0 +1,52 @@
+for i in (seq 0 9)
+	set -l mids
+	if test (math $i % 2) -eq 0
+		for suf in 0 1
+			set mids $mids fc{$suf}_{$i}_(seq 0 7)
+		end
+		for suf in 0 1
+			set mids $mids fd{$suf}_{$i}_(seq 0 7)
+		end
+		for suf in 0 1
+			set mids $mids gd{$suf}_{$i}_(seq 0 7)
+		end
+	else
+		for suf in 0 1 s
+			set mids $mids fc{$suf}_{$i}_(seq 0 7)
+		end
+	end
+
+	set -l groups
+	for k0 in (seq 0 1)
+		for k in (seq 0 7)
+			set mids_kk0 $mids
+			if test (math $i % 2) -eq 0
+				# set mids_kk0 $mids_kk0 conv{0,1}_{$i}_{$k}
+				set mids_kk0 $mids_kk0 mid00_{$i}_{$k} mid01_{$i}_{$k} mid02_{$i}_{$k} mid03_{$i}_{$k} mid04_{$i}_{$k} mid05_{$i}_{$k} mid06_{$i}_{$k}
+				set mids_kk0 $mids_kk0 conv0_{$i}_{$k}
+				set mids_kk0 $mids_kk0 mid10_{$i}_{$k} mid11_{$i}_{$k} mid12_{$i}_{$k} mid13_{$i}_{$k} mid14_{$i}_{$k} mid15_{$i}_{$k} mid16_{$i}_{$k}
+				set mids_kk0 $mids_kk0 conv1_{$i}_{$k}
+				if test $k0 -eq 0
+					set mids_kk0 $mids_kk0 convs_{$i}_{$k}
+				else
+					set mids_kk0 $mids_kk0 convd_{$i}_{$k}
+				end
+				set mids_kk0 $mids_kk0 m{$k0}_{$i}_{$k}
+				set mids_kk0 $mids_kk0 mm{$k0}_{$i}_{$k}
+				set mids_kk0 $mids_kk0 hh{$k0}_{$i}_{$k}
+				set mids_kk0 $mids_kk0 e{$k0}_{$i}_{$k}
+			else
+				set mids_kk0 $mids_kk0 conv{$k0}_{$i}_{$k}
+			end
+			set groups $groups (string join , $mids_kk0 arr{$i}{$k0}{$k}_c)
+			# set groups $groups (string join , $mids_kk0)
+		end
+	end
+
+	echo $i
+	echo $groups
+	# for some reason O3 before O1 is much faster...
+	# cv_cec -v -abc (which yosys-abc) O1dbg_neon_hints.cl O3_neon_hints.cl -ov (string join '#' $groups) -jobs 16 > all_mids_with_mm_with_arr/{$i}.log &
+	cv_cec -v -abc (which yosys-abc) O3_neon_hints.cl O1dbg_neon_hints.cl -ov (string join '#' $groups) -jobs 16 > all_mids_with_mm_with_arr__O3O1/{$i}.log &
+end
+wait
